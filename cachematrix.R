2d5d1cea95cf4ebff804eeb5f6c6ba4476bf3db3
Verma, Himanshu <superbhimanshu@gmail.com>
2015-10-15 20:39:07 -0400

    cacheSolve function implementation
    cache solve function is implemented in this commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,13 +1,10 @@
-## Put comments here that give an overall description of what your
 ##### makeCacheMatrix function returns a list of functions for:
 # 1. setting the matrix
 # 2. getting the matrix
 # 3. setting the inverse of matrix
 # 4. getting the inverse of matrix
 
-##### cacheSolve function does the following
-# 1. Computes inverse of the matrix created by makeCacheMatrix function if it doesn't exist in the cache already
-# 2. It returns the inverse of matrix from the cache if it already exists 
+
 ## functions do
 
 ## Write a short comment describing this function
@@ -19,11 +16,9 @@ makeCacheMatrix <- function(x = matrix()) {
 		x <<- y
 		inverseMatrix <<- NULL
 	}
-
 	get <- function() {
 		x
 	}
-
 	setInverse <- function(inv) {
 		inverseMatrix <<- inv
 	}
@@ -36,8 +31,24 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+##### cacheSolve function does the following
+# 1. Computes inverse of the matrix created by makeCacheMatrix function if it doesn't exist in the cache already
+# 2. It returns the inverse of matrix from the cache if it already exists 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        #create list of functions using makeCacheMatrix
+        listFuncs <- makeCacheMatrix(x)
+        listFuncs$set(x)
+        # check if inverse already exists in cache, if it does(not null) return it 
+        inverseMatrix <- listFuncs$getInverse()
+        if(!is.null(inverseMatrix)) {
+        	message("Inverse exists in cache")
+        	return(inverseMatrix)
+        }
+        #if it doesn't exist find inverse matrix and set the result to cache 
+        message("Inverse doesn't exist in cache. Estimating inverse and storing it in cache.")
+        inverseMatrix <- solve(x)
+        listFuncs$setInverse(inverseMatrix)
+        inverseMatrix
 }
